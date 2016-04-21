a126c35e0f77d46353a1b30c06d6034b9c2671b4
Jordan Zucker <jordan.zucker@gmail.com>
2016-04-21 16:08:30 -0700

    Starting to fill out sorter script

--- sorter.py
+++ sorter.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+import os
+from guessit import guessit
+
+directory_to_sort = '/Volumes/Xanadu-Plex/Old'
+destination_directory = '/Volumes/Xanadu-Plex'
+destination_movie_subdirectory = 'Movies'
+destination_tv_subdirectory = 'Television'
+
 def main():
 	print "main"
 
