4f483ad1a6113eebab301a78e39436ba69770298
Jordan Zucker <jordan.zucker@gmail.com>
2016-04-21 16:16:20 -0700

    Somewhat stubbed script

--- sorter.py
+++ sorter.py
@@ -5,11 +5,36 @@ from guessit import guessit
 
 directory_to_sort = '/Volumes/Xanadu-Plex/Old'
 destination_directory = '/Volumes/Xanadu-Plex'
-destination_movie_subdirectory = 'Movies'
-destination_tv_subdirectory = 'Television'
+
+class MediaFileCollector(object):
+	"""docstring for MediaFileCollector"""
+	def __init__(self, sorting_directory):
+		super(MediaFileCollector, self).__init__()
+		self.sorting_directory = sorting_directory
+	def all_files(self):
+		return []
+
+# automatically sorts movies into self.destination/Movies
+# and tv shows into self.destination/Television
+class Sorter(object):
+	"""docstring for Sorter"""
+	def __init__(self, destination):
+		super(Sorter, self).__init__()
+		self.destination = destination
+	def get_television_subdirectory(self):
+		return "blah"
+	def get_movies_subdirectory(self):
+		return "blah"
+	def sort(self, unsorted_files):
+		pass
+
+		
+		
 
 def main():
 	print "main"
+	collector = MediaFileCollector(directory_to_sort)
+
 
 if __name__ == "__main__":
 	main()
